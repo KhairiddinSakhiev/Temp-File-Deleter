8b283591731c6304ab354891f7d577522ce0854a
KhairiddinSakhiev <sakhievkhairiddin2000@gmail.com>
2022-04-25 10:08:17 +0500

    file deleter

---
+++ WorkWithFolders/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+var temp = Path.GetTempPath();
+var directs = Directory.GetDirectories(temp, "*.*", SearchOption.AllDirectories);
+while (true)
+{
+    foreach (string filePath in directs)
+    {
+        try
+        {
+            FileInfo currentFile = new FileInfo(filePath);
+            currentFile.Delete();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine("Error on file: {0}\r\n   {1}", filePath, ex.Message);
+        }
+    }
+
+    foreach (string filePath in directs)
+    {
+        try
+        {
+            DirectoryInfo currentDirect = new DirectoryInfo(filePath);
+            currentDirect.Delete(true);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine("Error on file: {0}\r\n   {1}", filePath, ex.Message);
+        }
+    }
+    await Task.Delay(1000);
+}
